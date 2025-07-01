26204d86bf5ea2ab9045b5910433096e526fad32
Kshitij Tiwari <kshitijtiwari.india@hotmail.com>
2025-07-01 19:14:10 +0530

    Update regarding ai response

--- DesktopAssistant.py
+++ DesktopAssistant.py
@@ -1,40 +1,39 @@
 import speech_recognition as sr
 import pyttsx3
 import datetime
-import wx
-import wikipedia
 import webbrowser
 import os
-import openai
+from google import genai
+from Key import api_key as key
 
-class desktopAssistant:
+class DesktopAssistant:
     def __init__(self):
-        engine = pyttsx3.init()
-        engine.say("O mai kenda sardar ji satsriakaal. How can I help you?")
-        print("O mai kenda sardar ji satsriakaal. How can I help you?")
-        engine.runAndWait()
+        self.engine = pyttsx3.init()
+        self.engine.setProperty("voice",'en-IN')
+        self.engine.setProperty("rate",160)
+        self.recognizer = sr.Recognizer()
+        self.talk("ooo mai kenhda serdaar ji sutsriakaal!! How do you do today?")
 
     def talk(self,text=str()):
-        engine = pyttsx3.init()
-        engine.say(text)
-        engine.runAndWait()
+        self.engine.say(text)
+        self.engine.runAndWait()
         print(text)
         return None
 
     def hear(self):
-        r = sr.Recognizer()
         with sr.Microphone() as source:
             print("Listening...")
-            audio = r.listen(source)
+            audio =self.recognizer.listen(source)
         try:
             print("Recognizing...")
-            query = r.recognize_google(audio, language='en-in')
+            query =self.recognizer.recognize_google(audio, language='en-in')
             print(f"User said: {query}\n")
         except Exception as e:
             print(e)
             self.talk("Say that again please...")
             return "None"
         return query.lower()
+    @staticmethod
     def browser(self,name=str()):
         if 'open youtube' in name:
             webbrowser.open("youtube.com")
@@ -46,23 +45,38 @@ class desktopAssistant:
             webbrowser.open("github.com")
         elif 'open gmail' in name:
             webbrowser.open("gmail.com")
-        return None
+        nameword = name.split()
+        website = nameword[(nameword.index("open")+1)]
+        self.talk(f"Opening {website}...")
+        return 1
+    @staticmethod
     def run(self,name=str()):
         words = name.split()
         i=words.index("open")
         app = words[i+1]
         os.system(app)
         return None
-desktopAssistant = desktopAssistant()
-i = 1
-while i==1:
-    query = desktopAssistant.hear()
+    def gptresp(self,query=str()):
+        client = genai.Client(api_key=key)
+        self.talk("Please wait while I am thinking...")
+        response = client.models.generate_content(
+            model="gemini-2.5-flash", contents=(query.lower()+" in about 100 words")
+        )
+        print(response.text)
+        return response.text.replace("*","")
+da = DesktopAssistant()
+while 1 == 1:
+    query = da.hear()
     if "time" in query:
         strTime = datetime.datetime.now().strftime("%H:%M:%S")
-        desktopAssistant.talk(f"Sir, the time is {strTime}")
-    if "what is" in query:
+        da.talk(f"Sir, the time is {strTime}")
     if "open" in query.lower():
-        desktopAssistant.browser(query)
-        desktopAssistant.run(query)
-
+        result = da.browser(query)
+        if result == 1:
+            da.run(query)
+    if "tell me about" or "what is" in query.lower():
+        answer = da.gptresp(query)
+        da.talk(answer)
+    if "exit" or "quit" in query:
+        break
 
